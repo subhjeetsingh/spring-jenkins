92f523d00d1a03fe3b8b9fcd51cd03931bf71b9f
subhjeet singh <jeetsmart8@gmail.com>
2024-04-25 09:16:53 +0530

    commit wth no error

--- src/test/java/com/subhjeet/spring/jenkins/api/SpringBootJenkinsDemoApplicationTests.java
+++ src/test/java/com/subhjeet/spring/jenkins/api/SpringBootJenkinsDemoApplicationTests.java
@@ -13,7 +13,7 @@ class SpringBootJenkinsDemoApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsDemoApplication.class);
 
 	@Test
-	contextLoads() {
+	void contextLoads() {
 		logger.info("Test Case Executing.....");
 		logger.info("Test Case Executing second log statement.....");
 		assertEquals(true, true);
